bca2c576cb8173a579f328a3f7a0394792dca886
SOUMYA1608 <soumyaponnam088@gmail.com>
2025-07-15 22:26:50 +0530

    Create viyastrings.java

---
+++ Java_fundamentals/viyastrings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+public class viyastrings{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("enter strings");
+        String s1 = sc.nextLine();
+        System.out.println(" wellcome " + s1);
+    }
+}
