684055d34e94630c45dfe797c6d75ab6681c5c33
SOUMYA1608 <soumyaponnam088@gmail.com>
2025-07-15 23:00:02 +0530

    Create Mumbai.java

---
+++ Java_fundamentals/Mumbai.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+public class Mumbai{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("enter 2 strings");
+        String s1 = sc.nextLine(); 
+        String s2 = sc.nextLine();
+        System.out.println(s1 + " technologies " + s2);
+    }
+}
